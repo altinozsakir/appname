9f7141824320d275f724cc6ca8f2f4d881d9c174
noname <altinozsakir@gmail.com>
2018-10-19 19:57:13 +0300

    react deleted

--- src/main/java/com/companyname/appname/controller/HomeController.java
+++ src/main/java/com/companyname/appname/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.companyname.appname.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
     @RequestMapping(value = "/")
-    public String index(){
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
         return "index";
     }
 }
